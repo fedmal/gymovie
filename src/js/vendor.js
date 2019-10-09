a3e34e0d08a60ca5d16c7c22d97fe8ed095f8e15
Dmitriy <fedmal.2012@gmail.com>
2019-10-09 11:04:00 +0500

    first commit

---
+++ src/js/vendor.js
@@ -0,0 +1,6 @@
+//= ../libs/jquery/dist/jquery.min.js
+//= ../libs/OwlCarousel2-2.3.4/dist/owl.carousel.js
+//= ../libs/slick-1.8.1/slick/slick.js
+//= ../libs/video-js/video.js
+//= ../libs/jQueryFormStyler/dist/jquery.formstyler.js
+//= ../libs/fancybox-master/dist/jquery.fancybox.js
